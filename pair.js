86a6ad4c85afd26becf1ca94ebf01499cc9bcf64
hammadshabbir0009t-cmyk <hammadshabbir0009t@gmail.com>
2026-05-29 08:18:57 +0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 let router = express.Router();
 const pino = require('pino');
 const {
-    default: Arslan_Tech,
+    default: Alone_Hacker,
     useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore,
@@ -22,10 +22,10 @@ router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
     
-    async function Arslan_MD_PAIR_CODE() {
+    async function ALONE_MD_PAIR_CODE() {
         const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
         try {
-            let Pair_Code_By_Arslan_Tech = Arslan_Tech({
+            let Pair_Code_By_Alone_Hacker = Alone_Hacker({
                 auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({ level: 'fatal' }).child({ level: 'fatal' })),
@@ -35,51 +35,50 @@ router.get('/', async (req, res) => {
                 browser: Browsers.macOS('Chrome')
             });
 
-            if (!Pair_Code_By_Arslan_Tech.authState.creds.registered) {
+            if (!Pair_Code_By_Alone_Hacker.authState.creds.registered) {
                 await delay(1500);
                 num = num.replace(/[^0-9]/g, '');
-                const code = await Pair_Code_By_Arslan_Tech.requestPairingCode(num);
+                const code = await Pair_Code_By_Alone_Hacker.requestPairingCode(num);
                 if (!res.headersSent) {
                     await res.send({ code });
                 }
             }
 
-            Pair_Code_By_Arslan_Tech.ev.on('creds.update', saveCreds);
-            Pair_Code_By_Arslan_Tech.ev.on('connection.update', async (s) => {
+            Pair_Code_By_Alone_Hacker.ev.on('creds.update', saveCreds);
+            Pair_Code_By_Alone_Hacker.ev.on('connection.update', async (s) => {
                 const { connection, lastDisconnect } = s;
                 if (connection === 'open') {
                     await delay(5000);
                     let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                     await delay(800);
                     let b64data = Buffer.from(data).toString('base64');
-                    let session = await Pair_Code_By_Arslan_Tech.sendMessage(Pair_Code_By_Arslan_Tech.user.id, { text: 'ARSLAN-MD~' + b64data });
+                    let session = await Pair_Code_By_Alone_Hacker.sendMessage(Pair_Code_By_Alone_Hacker.user.id, { text: 'ALONE-MD~' + b64data });
 
-                    let Arslan_MD_TEXT = `
+                    let Alone_MD_TEXT = `
         
 ╔════════════════════◇
 ║『 SESSION CONNECTED』
-║ ✨ Arslan-MD 🔷
-║ ✨ ArslanMD OFFICIAL🔷
+║ ✨ Alone-MD 🔷
+║ ✨ ALONE HACKER MODS🔷
 ╚════════════════════╝
 
 
 ---
 
 ╔════════════════════◇
-║『 YOU'VE CHOSEN Arslan-MD 』
+║『 YOU'VE CHOSEN ALONE-MD 』
 ║ -Set the session ID in Heroku:
 ║ - SESSION_ID: 
 ╚════════════════════╝
 ╔════════════════════◇
 ║ 『••• _V𝗶𝘀𝗶𝘁 𝗙𝗼𝗿_H𝗲𝗹𝗽 •••』
-║❍ 𝐎𝐰𝐧𝐞𝐫: 923237045919
-║❍ 𝐑𝐞𝐩𝐨: https://github.com/Arslan-MD/Arslan_MD
-║❍ 𝐖𝐚𝐆𝗿𝐨𝐮𝐩: https://chat.whatsapp.com/KRyARlvcUjoIv1CPSSyQA5?mode=wwt
-║❍ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: https://whatsapp.com/channel/0029VarfjW04tRrmwfb8x306
+║❍ 𝐎𝐰𝐧𝐞𝐫: 923235248941
+║❍ 𝐖𝐚channel: https://whatsapp.com/channel/0029Vb50wllJuyAEmYS7iV1h
+║❍ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞l: https://whatsapp.com/channel/0029Vb50wllJuyAEmYS7iV1h
 ║
 ║ ☬ ☬ ☬ ☬
 ╚═════════════════════╝
-𒂀 Enjoy Arslan-MD
+𒂀 Enjoy ALONE-MD
 
 
 ---
@@ -87,10 +86,10 @@ router.get('/', async (req, res) => {
 Don't Forget To Give Star⭐ To My Repo
 ______________________________`;
 
-                    await Pair_Code_By_Arslan_Tech.sendMessage(Pair_Code_By_Arslan_Tech.user.id, { text: Toxic_MD_TEXT }, { quoted: session });
+                    await Pair_Code_By_Alone_Hacker.sendMessage(Pair_Code_By_Alone_Hacker.user.id, { text: Toxic_MD_TEXT }, { quoted: session });
 
                     await delay(100);
-                    await Pair_Code_By_Arslan_Tech.ws.close();
+                    await Pair_Code_By_Alone_Hacker.ws.close();
                     return await removeFile('./temp/' + id);
                 } else if (connection === 'close' && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
@@ -106,7 +105,7 @@ ______________________________`;
         }
     }
     
-    return await Arslan_MD_PAIR_CODE();
+    return await ALONE_MD_PAIR_CODE();
 });
 
 module.exports = router;
